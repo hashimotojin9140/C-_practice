7f7513413829b07dfc460d4cc0f098f995de22d1
hashimotojin9140 <hashimotojin.ne.jp@gmail.com>
2022-06-02 22:43:57 +0900

    first commit

---
+++ youtube_practice/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace youtube_practice// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string newName;
+            newName = "Ryu";
+            Console.WriteLine(newName);
+
+            newName = "Ken";
+            Console.WriteLine(newName);
+        
+            int x = 1;
+            Console.WriteLine(x);
+            decimal y = 1.23m;
+            Console.WriteLine(y);
+            char c = 'a';
+            Console.WriteLine(c);
+            string s = "aiueo";
+            Console.WriteLine(s);
+            bool is_ok = true;
+            Console.WriteLine(is_ok);
+
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello  world!");
+            Console.WriteLine("Hello  world!");
+            Console.WriteLine("c:¥¥dir¥¥repos");
+            Console.WriteLine(@"#######
+Hello
+world !!!
+##########");
+            Console.WriteLine(@" ¥t¥n¥¥ ");
+        }
+            
+
+    }
+}
+
+
